9885479436c7a73bce3a69feb7180fe6425f6656
sarincr <sarinrays@gmail.com>
2021-08-18 19:23:17 +0530

    Just Py Fun

---
+++ JustPy/02.FUN/main.py
@@ -0,0 +1,14 @@
+import justpy as jp
+
+def my_click(self, msg):
+    self.text = 'I was clicked!'
+
+def hello_world():
+    wp = jp.WebPage()
+    d = jp.Div(text='Hello world!')
+    d.on('click', my_click)
+    wp.add(d)
+    return wp
+
+jp.justpy(hello_world)
+ 
